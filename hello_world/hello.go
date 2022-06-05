7dde7e565a297e0cca07077a7b40583529ba1a47
Akseli Nelander <anelander@gmail.com>
2022-06-05 19:03:42 +0200

    Hello, World

---
+++ hello_world/hello.go
@@ -0,0 +1,32 @@
+package hello
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(recipient string, language string) string {
+	if recipient == "" {
+		recipient = "World"
+	}
+	return getGreetingPrefix(language) + recipient
+}
+
+func getGreetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
